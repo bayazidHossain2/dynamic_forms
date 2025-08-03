676c3acdcceac48b4fcda25822021e3ce04bc359
bayazidHossain2 <bh.190140@s.pust.ac.bd>
2025-08-03 14:38:25 +0600

    Image view widget added on form view page

--- lib/features/form_list/presentation/widgets/image_list_view_widget.dart
+++ lib/features/form_list/presentation/widgets/image_list_view_widget.dart
@@ -1,24 +1,44 @@
 import 'dart:typed_data';
 
+import 'package:dynamic_forms/core/theme/app_pallete.dart';
+import 'package:dynamic_forms/features/form_list/domain/entities/field_properties.dart';
 import 'package:flutter/cupertino.dart';
 
 class ImageListViewWidget extends StatelessWidget {
+  final FieldProperties properties;
   final List<Uint8List> imageDataList;
 
-  const ImageListViewWidget({super.key, required this.imageDataList});
+  const ImageListViewWidget({
+    super.key,
+    required this.properties,
+    required this.imageDataList,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: imageDataList.length,
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      itemBuilder: (context, index) {
-        return Container(
-          margin: EdgeInsets.all(8),
-          child: Image.memory(imageDataList[index]),
-        );
-      },
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          properties.label.toString(),
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        ListView.builder(
+          itemCount: imageDataList.length,
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            return Container(
+              padding: EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                border: Border.all(color: AppPallete.primary, width: 2),
+              ),
+              margin: EdgeInsets.all(8),
+              child: Image.memory(imageDataList[index]),
+            );
+          },
+        ),
+      ],
     );
   }
 }
